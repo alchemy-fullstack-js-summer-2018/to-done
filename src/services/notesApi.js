7a0948b5d7f754de3993a4f4c4796fba7cb1c035
Mark Myers <markalope@gmail.com>
2018-08-21 16:53:16 -0700

    building out notes.api

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -1,4 +1,39 @@
 import { put, post, get, del } from './request';
+import { utimes } from 'fs';
 
 const URL = 'https://to-done-bf75b.firebaseio.com/';
 const NOTES_URL = `${URL}/notes`;
+
+const getNoteUrl = key => `${NOTES_URL}/${key}.json`;
+
+export const getNotes = () => {
+  return get(`${NOTES_URL}.json`)
+    .then(response => {
+      return response
+        ? Object.keys(response).map(key => {
+          const each = response[key];
+          each.key = key;
+          return each;
+        })
+        : [];
+    });
+};
+
+export const addNote = (note) => {
+  const url = `${NOTES_URL}.json`;
+  return post(url, note)
+    .then(res => {
+      note.key = res.title;
+      return note;
+    });
+};
+
+export const updateNote = note => {
+  const url = getNoteUrl(note.key);
+  return put(url, note);
+};
+
+export const removeNote = id => {
+  const url = getNoteUrl(id);
+  return del(url);
+};
