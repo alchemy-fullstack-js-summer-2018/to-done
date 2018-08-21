e4d5149f1aef4a015e10b4a45c126119e915e416
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-21 16:54:54 -0700

    app complete

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import NotesContainer from './animals/AnimalsContainer';
+import styles from './App.css';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <Router>
+        <div className={styles.app}>
+          <header>
+            <Header/>
+          </header>
+
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/notes" component={NotesContainer}/>
+              <Redirect to="/"/>
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
