6e51e465253228c2f5be9ab3ac014ead5e60d488
Mark Myers <markalope@gmail.com>
2018-08-22 11:14:37 -0700

    finished note.js

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -15,5 +15,45 @@ class Note extends Note {
     onUpdate: PropTypes.func.isRequired
   };
 
+  handleEdit = () => {
+    this.setState({ editing: true });
+  };
 
+  handleEndEdit = () => {
+    this.setState({ editing: false });
+  };
+
+  handleDelete =() => {
+    const { note, onRemove } = this.props;
+    return onRemove(note.key);
+  };
+
+  handleComplete = note => {
+    const { onUpdate } = this.props;
+    return onUpdate(note).then(this.handleEndEdit);
+  };
+
+  render() {
+    const { editing } = this.state;
+    const { note } = this.props;
+
+    return (
+      <li>
+        {editing
+          ? <NoteForm
+            note={note}
+            onComplete={this.handleComplete}
+            onCancel={this.handleEndEdit}
+          />
+          : <NoteDisplay
+            note={note}
+            onEdit={this.handleEdit}
+            onDelete={this.handleDelete}
+          />
+        }
+      </li>
+    );
+  }
 }
+
+export default Note;
