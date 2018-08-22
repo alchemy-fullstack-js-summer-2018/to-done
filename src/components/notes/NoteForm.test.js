c1fd3befd3ddf6e7ac8d253addc13b00c1f2e04a
rbtprograms <rbt61690@gmail.com>
2018-08-22 10:52:47 -0700

    noteform test continuing to pass

--- src/components/notes/NoteForm.test.js
+++ src/components/notes/NoteForm.test.js
@@ -12,5 +12,33 @@ describe('Note Form', () => {
 
     const wrapper = mount(<NoteForm onComplete={handleComplete}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
+
+    const note = {
+      name: 'note1',
+      message: 'finish your lab'
+    };
+
+    wrapper.find('input[name="name"]').simulate('change', {
+      target: {
+        name: 'name',
+        value: note.name
+      }
+    });
+
+    wrapper.find('input[name="message"]').simulate('change', {
+      target: {
+        name: 'message',
+        value: note.message
+      }
+    });
+
+    wrapper.find('button').simulate('submit');
+
+    const calls = handleComplete.mock.calls;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toEqual(note);
+
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
   });
 });
