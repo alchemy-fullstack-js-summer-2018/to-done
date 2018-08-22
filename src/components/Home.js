04f3b409c50af9bef7905f15d6b2aa410c0139e7
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-21 18:20:25 -0700

    notes container added

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ class Home extends Component {
     return (
       <div>
         <h2>Home Page</h2>
+        <h4>App Summary</h4>
+        <p>This is an app that you can use to create <em>To Do</em> lists. You can create, edit, or delete your list too! Have fun and goodluck!</p>
+        <hr></hr>
         <p>edit list of notes</p>
       </div>
     );
