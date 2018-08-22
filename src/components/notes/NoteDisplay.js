fe27bdfc3bfd781f6dfe295b4e78ca6f358b4c94
Mario Quintana <pyrros2000@gmail.com>
2018-08-21 18:11:26 -0700

    NoteDisplay test passing...

---
+++ src/components/notes/NoteDisplay.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Note extends Component {
+
+  static propTypes = {
+    note: PropTypes.object.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { note, onEdit, onDelete } = this.props;
+
+    return (
+      <p>
+        {note.name} the {note.type}
+        <button name="edit" onClick={onEdit}>✎</button>
+        <button name="delete" onClick={onDelete}>🗑</button>
+      </p>
+    );
+  }
+}
+
+export default Note;
