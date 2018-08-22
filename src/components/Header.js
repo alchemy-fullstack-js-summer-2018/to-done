d58cdc295a022fb3c053e8f44a6722aa2824f2cc
MariahAdams <mariah.adams23@yahoo.com>
2018-08-22 08:58:36 -0700

    added home component, working on scaffolding for header

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+// import styles from './Header.css';
+import { Route, NavLink } from 'react-router-dom';
+
+class Header extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink>
+                
+              </NavLink>
+            </li>
+            <li>
+              <NavLink>
+
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import styles from './Home.css';
+
+class Home extends Component {
+
+  render() {
+
+    return (
+      <h2>Welcome Home!</h2>
+    );
+  }
+}
+
+export default Home;
