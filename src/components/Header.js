ba7586cfe93a1587d236bcdaa9bb49bd9256d7d3
Mario Quintana <pyrros2000@gmail.com>
2018-08-21 17:54:28 -0700

    Simple app, home, links, and header created that render on webpage

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.css';
+
+class Header extends Component {
+
+  static propTypes = {
+
+  };
+
+  render() {
+
+    return (
+      <div className={styles.header}>
+      
+        <section>
+          <div className="logo">
+            <h1>Notes</h1>
+          </div>
+
+          <nav>
+            <ul>
+              <li>
+                <NavLink exact activeClassName="active" to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink exact activeClassName="active" to="/notes">Notes</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <p>edit lists of notes</p>
+      </div>
+    );
+  }
+}
+export default Home;
