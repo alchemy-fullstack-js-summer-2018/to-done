1e6aa3f1e812483fe6c6695393fb961ae7a6dcb3
Mario Quintana <pyrros2000@gmail.com>
2018-08-21 17:35:24 -0700

    Created and modified notesApi

---
+++ src/services/notesApi.js
@@ -0,0 +1,33 @@
+import { put, post, get, del } from './request';
+const URL = 'https://todo-9ea8e.firebaseio.com';
+const NOTES_URL = `${URL}/notes`;
+const getNoteUrl = key => `${NOTES_URL}/${key}.json`;
+
+export const getNotes = () => {
+  return get(`${NOTES_URL}.json`)
+    .then(response => {
+      return response
+        ? Object.keys(response).map(key => {
+          const each = response[key];
+          each.key = key;
+          return each;
+        })
+        : [];
+    });
+};
+
+export const addNote = (note) => {
+  const url = `${NOTES_URL}.json`;
+  return post(url, note)
+    .then(res => {
+      note.key = res.name;
+    });
+};
+export const updateNote = note => {
+  const url = getNoteUrl(note.key);
+  return put(url, note);
+};
+export const removeNote = id => {
+  const url = getNoteUrl(id);
+  return del(url);
+};
