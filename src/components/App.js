ab000f689b6ff19bfec09f76d2fc0f9e9d8c2e66
Rehmsy <sarahrehmer@gmail.com>
2018-08-21 17:24:28 -0700

    need to close vscode

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './Header';
+import Home from './/Home';
+import styles from './App.css';
+
+class App extends Component {
+    
+  render() {
+    
+    return (
+      <Router>
+        <div className={styles.app}>
+          <header>
+            <Header/>
+          </header>
+      
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/search" component={Container}/>
+              <Redirect to="/"/>
+            </Switch> 
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
