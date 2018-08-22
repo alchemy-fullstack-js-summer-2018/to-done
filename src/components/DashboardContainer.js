8ff6819c851839df33863108b1656b2969b18622
Arthur Jen <arthurljen@gmail.com>
2018-08-21 17:08:23 -0700

    added nav for dashboard and home components

---
+++ src/components/DashboardContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DashboardContainer extends Component {
+  render() {
+    return (
+      <h2>Dashboard Container</h2>
+    );
+  }
+}
+
+export default DashboardContainer;
