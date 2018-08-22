6fe27ffed46dd20f2f43e3b5ac66b57a7eb66c5f
MariahAdams <mariah.adams23@yahoo.com>
2018-08-22 09:38:53 -0700

    form added to note form

--- src/components/Dashboard/NoteForm.js
+++ src/components/Dashboard/NoteForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 class NoteForm extends Component {
 
@@ -7,9 +8,27 @@ class NoteForm extends Component {
     return (
       <div>
         <h3>I am the note form</h3>
+        <form onSubmit={this.handleSubmit}>
+          {/* <InputControl name="title" value="" />
+          <InputControl name="notes" value="" /> */}
+          <label>Title:<input type="text"></input></label>
+          <label>Notes:<textarea rows="4" cols="50"></textarea></label>
+          <p>
+            <button type="submit">Submit</button>
+          </p>
+        </form>
       </div>
     );
   }
 }
+
+// const InputControl = (props) => (
+//   <p>
+//     <label>
+//       {props.name}:
+//       <input {...props} required />
+//     </label>
+//   </p>
+// );
 
 export default NoteForm;
