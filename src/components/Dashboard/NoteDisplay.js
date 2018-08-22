4be38fd0e1479a2a4d94985c13776b8c8c6372f0
MariahAdams <mariah.adams23@yahoo.com>
2018-08-22 11:11:51 -0700

    note content bug fixed, notes are now rendering to the screen

--- src/components/Dashboard/NoteDisplay.js
+++ src/components/Dashboard/NoteDisplay.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class NoteDisplay extends Component {
 
+  static propTypes = {
+    note: PropTypes.object.isRequired
+  };
+
   render() {
+    const { note } = this.props;
 
     return (
-      <div>
-        <h4>NoteDisplay Component</h4>
-      </div>
+      <p>
+        <span>{note.title}</span>: {note.notes}, {note.completed} 
+      </p>
     );
   }
 }
