b29670a9ffaca85a5469513e2b5ddeeb80ed6033
MariahAdams <mariah.adams23@yahoo.com>
2018-08-22 09:24:27 -0700

    wired up NoteForm component

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-// import styles from './Notes.css';
+import NoteForm from  './NoteForm';
+import styles from './Notes.css';
 
 class DashboardContainer extends Component {
+
+  state = {
+    notes: null
+  };
+
+  componentDidMount() {
+    console.log('component did mount!');
+  }
+
+  addNote(note) {
+    console.log('note added', note);
+  }
 
   render() {
 
     return (
-      <h2>Welcome to the Dashboard!</h2>
+      <div className={styles.notes}>
+        <h2>Welcome to the Dashboard!</h2>
+
+        <NoteForm/>
+      </div>
     );
   }
 }
---
+++ src/components/Dashboard/NoteForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NoteForm extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h3>I am the note form</h3>
+      </div>
+    );
+  }
+}
+
+export default NoteForm;
