25881d8b50aab9fcc4911fcf29ce75d37afb2732
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-08-21 21:49:56 -0700

    Add initial App and Landing page content

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Landing from './Landing';
+import DashboardContainer from './dashboard/DashboardContainer';
+
+class App extends Component {
+  render() { 
+    return (
+      <Router>
+        <div>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Landing}/>
+              <Route exact path="/dashboard" component={DashboardContainer}/>
+              <Redirect to="/"/>
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+ 
+export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() { 
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <p>Woo todooooooos</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
