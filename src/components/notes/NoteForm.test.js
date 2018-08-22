62927b3e17392cba317620fcb152defb7ca5fc87
Easton Gorishek <ieaston26@gmail.com>
2018-08-22 12:37:36 -0700

    added jest testing on edit portion of form

--- src/components/notes/NoteForm.test.js
+++ src/components/notes/NoteForm.test.js
@@ -46,4 +46,46 @@ describe('Note form', () => {
         expect(toJSON(wrapper)).toMatchSnapshot();
       });
   });
+
+  it('renders edit if note prop', () => {
+    const handleComplete = jest.fn();
+    const promise = Promise.resolve();
+    handleComplete.mockReturnValueOnce(promise);
+    const handleCancel = jest.fn();
+
+    const note = { 
+      key: 'key', 
+      title: 'A note', 
+      content: 'A nice note.',
+      completed: false
+    };
+
+    const wrapper = mount(<NoteForm
+      onComplete={handleComplete}
+      onCancel={handleCancel}
+      note={note}
+    />);
+
+    wrapper.find('textarea[name="content"]').simulate('change', {
+      target: {
+        name: 'content',
+        value: 'A great note!'
+      }
+    });
+
+    wrapper.find('button[type="submit"]').simulate('submit');
+
+    const calls = handleComplete.mock.calls;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toEqual({
+      ...note,
+      content: 'A great note!'
+    });
+
+    return promise
+      .then(() => {
+        wrapper.update();
+        expect(toJSON(wrapper)).toMatchSnapshot();
+      });
+  });
 });
