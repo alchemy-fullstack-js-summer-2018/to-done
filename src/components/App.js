82aff524971ed5a24722d65a5f3fdbb2c8c4e10d
MariahAdams <mariah.adams23@yahoo.com>
2018-08-21 17:01:05 -0700

    added router to app and created header component file

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import styles from './App.css'
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+// import Landing from './Landing/Landing';
+import styles from './App.css';
 
 class App extends Component {
 
   render() {
 
     return (
-      <main className={styles.app}>
-        <h1>Hello World</h1>
-      </main>
-    )
+      <Router>
+        <Fragment>
+          <header className={styles.app}>
+            <h1>Hello World</h1>
+          </header>
+
+          <main>
+            <Switch>
+              <Route exact path="/"/*  component={Landing} *//>
+              <Redirect to="/"/>
+            </Switch>
+          </main>
+        </Fragment>
+      </Router>
+    );
   }
 }
 
