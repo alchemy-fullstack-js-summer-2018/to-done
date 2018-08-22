673402bd8907f0edb8887f70a1c5630f654c7f53
MariahAdams <mariah.adams23@yahoo.com>
2018-08-22 10:01:38 -0700

    working on wiring up note component

---
+++ src/components/Dashboard/Note.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Note extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h4>Note</h4>
+      </div>
+    );
+  }
+}
+
+export default Note;
--- src/components/Dashboard/NoteList.js
+++ src/components/Dashboard/NoteList.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Note from './Note';
 
 class NoteList extends Component {
 
+  static propTypes = {
+    notes: PropTypes.array
+  };
+
   render() {
+    const { notes } = this.props;
 
     return (
       <div>
         <h3>I am the Note List</h3>
+        <ul>
+          {notes.map(note => (
+            <Note
+              key={note.key}
+              note={note}
+            />
+          ))}
+        </ul>
       </div>
     );
   }
