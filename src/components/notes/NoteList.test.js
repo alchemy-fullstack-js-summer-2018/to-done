ae2c4048a4f733f88f084abec6d2ed8754db4621
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-23 14:09:36 -0700

    Snapshot test for NoteList successful

--- src/components/notes/NoteList.test.js
+++ src/components/notes/NoteList.test.js
@@ -6,8 +6,24 @@ import NoteList from './NoteList';
 describe('Note List', () => {
 
   it('Renders display', () => {
+    const onUpdate = jest.fn();
+    const promise = Promise.resolve();
+    onUpdate.mockReturnValueOnce(promise);
+
+    const onRemove = jest.fn();
+    onRemove.mockReturnValueOnce(promise);
+
+    const note = { 
+      key: 'abc', 
+      title: 'Note', 
+      content: 'Content', 
+      completed: false 
+    };
+
     const wrapper = shallow(<NoteList
-      note={{ title: 'Note', content: 'Content' }}
+      note={note}
+      onUpdate={onUpdate}
+      onRemove={onRemove}
     />);
 
     expect(toJSON(wrapper)).toMatchSnapshot();
